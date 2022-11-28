0b0da5d7ef94c15a6d455f49c15024e86658dc3e
Noy <noy_gpg.2fcap@simplelogin.com>
2022-11-28 12:37:04 +0800

    🐳 chore: add filter

--- site.config.ts
+++ site.config.ts
@@ -6,7 +6,7 @@ export default siteConfig({
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
-  rootNotionSpaceId: null,
+  rootNotionSpaceId: '511f7d9e-5e9f-4501-92ac-1a78f4217dd9',
 
   // basic site info (required)
   name: 'Notion faster',
